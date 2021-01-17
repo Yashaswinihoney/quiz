ea7b37bf79be941fcc25d47120dffc7bb164e343
kanishkaditya shukla <66269417+kanishkaditya@users.noreply.github.com>
2021-01-17 17:44:20 +0530

    animation added and quiz classes added

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,103 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Navigation_Drawer.dart';
+import 'package:flutter_application_1/QuizRepostory.dart';
+import 'package:flutter_application_1/s2.dart';
+import 'package:flutter_application_1/s3.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Home());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Quiz',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home:  MyApp(),
+      routes: <String, WidgetBuilder>{
+        '/s3':(BuildContext context) => new s3(),
+        '/home':(BuildContext context) => new MyApp()
+      },
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => s2(),
+    transitionDuration: Duration(milliseconds: 500),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+    var begin = Offset(0.0, 1.0);
+    var end = Offset.zero;
+    var curve = Curves.ease;
+
+    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+    return SlideTransition(
+      position: animation.drive(tween),
+      child: child
+    );
+  },
+  );
+}
+
+
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  QuizRepository _quizRepository=QuizRepository();
+
+  int quiz_id=1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
             appBar: AppBar(
               title: Text(
                 "Quiz",
               ),
             ),
-            drawer: Drawer(
-                child:
-                    ListView(padding: EdgeInsets.zero, children: const <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.message),
-                title: Text('Messages'),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('Profile'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-            ])),
+
+            drawer: Navigation_Drawer(),
             body: Center(
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    alignment: Alignment.center,
-                    height: 200,
-                    width: 200,
-                    margin: EdgeInsets.all(20),
-                    child: Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu nisl nec mauris faucibus luctus non et neque. Donec rhoncus risus sit amet tellus pretium, eget condimentum nisl tempus. Ut interdum, nisl mattis gravida placerat, purus libero tristique libero, in consequat ipsum velit sed elit.",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.teal,
-                      gradient:
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        alignment: Alignment.center,
+                        height: 200,
+                        width: 200,
+                        margin: EdgeInsets.all(20),
+                        child: Text(_quizRepository.get_quiz(quiz_id).quiz_description),
+                        decoration: BoxDecoration(
+                          color: Colors.teal,
+                          gradient:
                           LinearGradient(colors: [Colors.red, Colors.pink]),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
-                    ),
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Domain1"),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
                         ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Domain1"),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Domain1"),
-                        ),
-                      ]),
-                  MaterialButton(
-                    color: Colors.blue,
-                    height: 40,
-                    minWidth: 40,
-                    splashColor: Colors.cyan,
-                    onPressed: () {},
-                    child: Icon(Icons.play_arrow),
-                  )
-                ]))));
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {quiz_id=1;setState(() {
+
+                              });},
+                              child: Text("Domain1"),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Domain1"),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Domain1"),
+                            ),
+                          ]),
+                      MaterialButton(
+                        color: Colors.blue,
+                        height: 40,
+                        minWidth: 40,
+                        splashColor: Colors.cyan,
+                        onPressed: () {Navigator.of(context).pushReplacement(_createRoute());},
+                        child: Icon(Icons.play_arrow),
+                      )
+                    ])));
   }
 }
